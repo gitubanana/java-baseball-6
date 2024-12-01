a29ddde03d5b4ff9ea460847606fb0f5840a9c63
gitubanana <ptyolo666@gmail.com>
2024-12-01 15:28:08 +0900

    feat(BaseballProperty): 야구 게임 관련 정보 상수화

---
+++ src/main/java/baseball/constant/BaseballProperty.java
@@ -0,0 +1,7 @@
+package baseball.constant;
+
+public class BaseballProperty {
+    public static final int BASEBALL_NUMBER_COUNT = 3;
+    public static final String YES_MESSAGE = "1";
+    public static final String NO_MESSAGE = "2";
+}
